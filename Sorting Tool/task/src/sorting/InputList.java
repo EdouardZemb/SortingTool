e272232f0d83a7a2856b918a953fdbbf2a86cdc7
Edouard Zemb <edouard.zemb@gmail.com>
2022-12-29 09:38:01 +0100

    fix: Percentage computation and string formatting

--- Sorting Tool/task/src/sorting/InputList.java
+++ Sorting Tool/task/src/sorting/InputList.java
@@ -22,16 +22,25 @@ public abstract class InputList<T extends Comparable<T>> {
 
     @Override
     public String toString() {
-
-        return "Total numbers: " +
+        return "Total " + dataType.name().toLowerCase() + ": " +
                 inputList.size() +
                 ".\nThe " +
                 (dataType == DataType.LONG ? "greatest " : "longest ") +
                 dataType.name().toLowerCase() +
-                " : " +
+                " :" +
+                (dataType == DataType.LINE ? "\n" : " ") +
                 maxValue() +
-                " (" +
-                Collections.frequency(inputList, maxValue()) +
-                " times(s)).";
+                (dataType == DataType.LINE ? "\n" : " ") +
+                "(" +
+                getFrequency() +
+                " times(s), " +
+                (int) (((double) getFrequency() / (double) inputList.size()) * 100D) +
+                "%).";
     }
+
+    private int getFrequency() {
+        return Collections.frequency(inputList, maxValue());
+    }
+
+
 }
